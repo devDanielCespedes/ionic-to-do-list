8b17f792786012cf297ca1e659565ba99c6a7440
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2025-02-27 16:40:20 -0300

    Replace theme toggle component to features/tasks folder

--- src/features/tasks/components/ThemeToggle/ThemeToggle.tsx
+++ src/features/tasks/components/ThemeToggle/ThemeToggle.tsx
@@ -1,12 +1,16 @@
 import { IonButton, IonIcon } from "@ionic/react";
 import { moon, sunny } from "ionicons/icons";
-import { useTheme } from "../../theme/hooks/useTheme";
+import { useTheme } from "../../../../theme/hooks/useTheme";
 
 export function ThemeToggle() {
   const { isDark, toggleTheme } = useTheme();
 
   return (
-    <IonButton onClick={toggleTheme}>
+    <IonButton
+      onClick={toggleTheme}
+      aria-label={isDark ? "Switch to light mode" : "Switch to dark mode"}
+      fill="clear"
+    >
       <IonIcon slot="icon-only" icon={isDark ? sunny : moon} />
     </IonButton>
   );
