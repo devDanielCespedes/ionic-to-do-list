f2a67e457a1ccfc0901e7443df8fd6d8b9c5f0f4
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2025-02-28 00:10:49 -0300

    Add LoadingOverlay component
    Part of #14

---
+++ src/features/tasks/components/LoadingOverlay/LoadingOverlay.tsx
@@ -0,0 +1,20 @@
+import { IonSpinner } from "@ionic/react";
+import { createPortal } from "react-dom";
+import styles from "./LoadingOverlay.module.css";
+
+interface LoadingOverlayProps {
+  isOpen: boolean;
+}
+
+export function LoadingOverlay({ isOpen }: LoadingOverlayProps) {
+  if (!isOpen) return null;
+  //https://react.dev/reference/react-dom/createPortal#:~:text=createPortal%20lets%20you%20render%20some%20children%20into%20a%20different%20part%20of%20the%20DOM.
+  return createPortal(
+    <div className={styles.overlay}>
+      <div className={styles.spinnerContainer}>
+        <IonSpinner name="crescent" className={styles.spinner} />
+      </div>
+    </div>,
+    document.body, // 🔹 Isso garante que o overlay cubra tudo
+  );
+}
